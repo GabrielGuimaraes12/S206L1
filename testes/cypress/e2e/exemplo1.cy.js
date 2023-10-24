4b6664103689ad32abfbb7b60effc869ec9d0e50
GabrielGuimaraes <Gabriel.filhagosa@ges.inatel.br>
2023-10-24 19:45:24 -0300

    aula3

--- testes/cypress/e2e/exemplo1.cy.js
+++ testes/cypress/e2e/exemplo1.cy.js
@@ -1,5 +1,34 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+/// <reference types="cypress"/>
+
+// Função para preencher o formulário de registro
+function preencherFormulario(nome, sobrenome, username, senha) {
+  cy.get('.btn-link').click()
+  cy.get('#firstName').type(nome)
+  cy.get('#Text1').type(sobrenome)
+  cy.get('#username').type(username)
+  cy.get('#password').type(senha)
+}
+function SubtrairSenha(senha) {
+  cy.get('#password').clear();
+}
+
+describe("Criando cenário de teste para o site globalsqa", () => {
+  it('Caso de teste: Registrar no site com sucesso', () => {
+   
+    cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login')
+
+    preencherFormulario("Gabriel", "Guimaraes", "Guima", "Guima")
+
+    cy.get('.btn-primary').click()
+    cy.get('.ng-binding').should('contain.text', 'Registration successful')
+  })
+
+  it('Caso de teste: Tentar registrar sem senha', () => {
+    cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login')
+
+    
+    preencherFormulario("Gabriel", "Guimaraes", "Guima", "2012")
+    SubtrairSenha("2012")
+    cy.get('.has-error > .help-block').should('contain.text', 'Password is required')
   })
 })
